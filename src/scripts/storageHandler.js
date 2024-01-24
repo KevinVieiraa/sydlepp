7927a59ddffaef2cf8dd76a76f9e39c0ff5b05fe
Kevin <kevin.vieira20@gmail.com>
2024-01-24 08:58:58 -0300

    Correção - posição do cursor desalinhada e carregamento da árvore de classes

--- src/scripts/storageHandler.js
+++ src/scripts/storageHandler.js
@@ -8,7 +8,7 @@ function loadClassTree() {
     
     let token = tokenStorage.main.users[0].accessToken.token;
     try {
-        $.ajax(`/api/1/main/_system/_workspace/getClassList?accessToken=${token}`)
+        $.ajax(`/api/1/main/_system/_workspace/getClassList?accessToken=${token}&_body=%7B%22_id%22%3A%22000000000000000000000053%22%2C%22viewId%22%3A%22000000000000000000000049%22%7D`)
         .done(function (response) {
             let classTree = response.result;
             localStorage.setItem('classTree', JSON.stringify(classTree));
